b3832fa9da783c6cc71c1ca2027f31393d7dce97
DESKTOP-LB3HQR1\USER <sangmin802@naver.com>
2021-05-13 14:42:28 +0900

    Socket 타입 userID 속성 추가

--- private-chat/private-chat/src/socket/index.tsx
+++ private-chat/private-chat/src/socket/index.tsx
@@ -1,8 +1,11 @@
-import { io } from "socket.io-client";
+import { io, Socket } from "socket.io-client";
 
 const url = "http://localhost:3001/";
+interface NSocket extends Socket {
+  userID?: string;
+}
 // socket 바로연결이 아닌, socket.connect()로 필요할 때만 연결 가능하게 함
-export const socket = io(url, { autoConnect: false });
+export const socket: NSocket = io(url, { autoConnect: false });
 
 // 클라이언트 socket의 모든 수신 이벤트들에 대해 콘솔을 찍도록 함
 //  ex) on으로 받아오는것들
