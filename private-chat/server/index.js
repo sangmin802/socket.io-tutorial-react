3fd150e86f8514a65c9546c40e3184e761e26e88
DESKTOP-LB3HQR1\USER <sangmin802@naver.com>
2021-05-13 16:22:08 +0900

    클라이언트의 localStorage와 서버의 socketStorage간 교류

--- private-chat/server/index.js
+++ private-chat/server/index.js
@@ -69,49 +69,53 @@ io.on("connection", socket => {
     },
   ];
 
+  // socket에 접속한 클라이언트 저장
+  sessionStore.saveSession(socket.sessionID, {
+    userID: socket.userID,
+    userName: socket.userName,
+    connected: true,
+  });
+
   // 클라이언트에게 생성된 sessionID, userID 전달
-  //  클라이언트에서는 전달된 값 localStorage에 저장
+  // 클라이언트에서는 전달된 값 localStorage에 저장
   socket.emit("session", {
     sessionID: socket.sessionID,
     userID: socket.userID,
   });
 
-  // io.of('/').sockets : 현재 socket에 접속한 모든 유저 리스트 생성
-  for (let [id, socket] of io.of("/").sockets) {
-    users.push({ userID: id, userName: socket.userName });
-  }
+  // 생성되는 socket간 메시지 전달이 아닌, 접속한 userID를 기반으로
+  // 상호 메시지 전달을 위해, userID Room 생성
+  // 기존 socket방식에서 socket.메소드로 자신에게 다시 보내는것은 불가능한가봄
+  socket.join(socket.userID);
 
-  // 접속중인 유저 리스트 송신
-  socket.emit("users", users);
-
-  // 새로 접속한 유저 송신
-  socket.broadcast.emit("user connected", {
-    userID: socket.id,
-    userName: socket.userName,
+  // socket에 접속한 혹은 접속했던 모든 클라이언트 반환
+  sessionStore.findAllSessions().forEach(session => {
+    users.push({
+      userID: session.userID,
+      userName: session.userName,
+      connected: session.connected,
+    });
   });
 
-  // userID기반의 room 입장
-  socket.join(socket.userID);
+  // socket의 모든 클라이언트 전달
+  socket.emit("users", users);
+
+  // 다른 유저 접속
+  socket.broadcast.emit("user connected", {
+    userID: socket.userID,
+    userName: socket.userName,
+    connected: true,
+  });
 
   // Room 개념
   // 선택한 유저에게 보내진 메시지 송신 후, 해당 유저에게 메시지 전달
   // 메시지 전달 테스트를 위한 데모유저의 응답
   // 고유 id에 보낼때랑, 생성된 채널(room)에 보낼때랑 다른듯함
-  // io.to : 귓속말 개념
-  // socket.to : 카톡 단체 채팅방 개념
-  //    - room방식으로, join, leave 메소드가 존재함
-  //    - socket에서 저장하지 않기 때문에, room data는 db로 관리해야하는듯
-  //    - 접속할때마다 room data를 가져와서 연결하는 느낌
+  // to 대상자가 없으면 지정된 이벤트를 감지하는 모두가 보는 전체채팅?
+  // userID를 쓰면 귓속말 개념으로 할 수 있을듯
+  // roomID를 생성하면 카톡 단체 채팅방 개념으로 할 수 있을듯
+  //  대화 내용과 roomID은 db로 저장?
   socket.on("private message", ({ content, to }) => {
-    // socket 서버에 받은 메시지를 수신자에게만 전달
-    // io.to(socket.id).emit("private message", {
-    //   content: `You said ${content}`,
-    //   from: to,
-    // });
-    // io.to(socket.id).emit("private message", {
-    //   content: "Hi Sangmin! What are you doing!",
-    //   from: "demo user 3",
-    // });
     // socket 서버에 받은 메시지를 전달자와 수신사 모두에게 전달
     io.to(to).to(socket.userID).emit("private message", {
       content,
