aac2c1a9e5587ee3bef8ee015be418aaf11e29f3
DESKTOP-LB3HQR1\USER <sangmin802@naver.com>
2021-05-14 16:48:55 +0900

    socket.io 로드밸런싱 관련

--- private-chat/server/cluster.js
+++ private-chat/server/cluster.js
@@ -3,19 +3,20 @@
 // scale out : 서버를 여러개 증설하여 클라이언트 분산(로드밸런싱)
 //  -로드밸런싱 작업
 //    클라이언트의 수가 늘어났을 때, 서버로 작업이 바로 전달되는것이 아닌, 로드밸런싱을 거쳐서 작업들이 분산되어 여러개의 서버에 전달됨
+//    aws ec2 auto scaling
 
 // 여러개의 socket.io서버를 만들 때 필요한 두가지 조건
 //  1. 고정 세션 활성화
-//      서로 다른 socket.id가 서로의 이벤트를 전달받을 수 있도록 해야하는것 같음
-//      따라서, 여러개의 socket.io 서버로 전달되는 클라이언트의 요청이 하나의 socket.io 서버로 라우팅 되도록 해야한다고 함
 //  2. 기본 메모리 어뎁터를 redis 어뎁터로 변경
 
-// cluster - 서버증설
+// cluster - 작업 스레드 증설 / 로드밸런싱 node.js 버전?
 //    메인 프로세스에 여러개의 자식 프로세스들이 생성됨
 //      이 하위 프로세스들을 worker 라고 하는듯 함. fork 이벤트로 생성
+
+// redis - 데이터 통신 활성화
+//    동일한 socket.io서버가 아니라면 emit등의 통신을 할 수 없다. 이러한점을 가능하게 해주는것이
+//    redis의 pub/sub 기능
 const cluster = require("cluster");
-const express = require("express");
-const app = express();
 const WORKDERS_COUNT = 4;
 
 // 메인 프로세스
